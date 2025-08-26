c614b201295e2da0c780b40b0c4ef553724c38b5
F Oliver Sumari H <49208086+fsumari@users.noreply.github.com>
2025-08-25 19:22:43 -0500

    Update main.py

--- service_api/main.py
+++ service_api/main.py
@@ -56,10 +56,10 @@ class IPRestrictionMiddleware(BaseHTTPMiddleware):
 # Uso con múltiples IPs
 
 # IPs de prueba en local
-#AUTHORIZED_IPS = ["127.0.0.1", "190.239.73.213", "192.168.110.209", "192.168.110.127", "192.168.100.190"]
+#AUTHORIZED_IPS = ["127.0.0.1", "190.239.73.213"]
 
 # AUTHORIZED_IPS = ["PRD OM", "OM DEV CHATBOT"]
-AUTHORIZED_IPS = ["35.232.156.102", "190.113.29.10"]
+AUTHORIZED_IPS = ["35.232.156.102", "190.113.29.10", "192.168.110.209", "192.168.110.127", "192.168.100.190"]
 origins = ["*"]
 
 # Middlewares
